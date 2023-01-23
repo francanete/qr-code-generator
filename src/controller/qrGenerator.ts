4521ea60f8af22bbf6d605c737264367d9cc2e41
francanete <francanetedev@gmail.com>
2023-01-23 17:19:55 +0000

    fix: calculate url length and apply corresponding qr version

--- src/controller/qrGenerator.ts
+++ src/controller/qrGenerator.ts
@@ -3,6 +3,21 @@ import qrcode from "qrcode";
 
 export const qrGenerator = async (req: Request, res: Response) => {
   const { url } = req.query as { url: string };
+
+  const urlLength = url.replace(/[-/]/g, "").length;
+
+  let version = 0;
+
+  if (urlLength > 250) {
+    version = 10;
+  } else if (urlLength > 200) {
+    version = 8;
+  } else if (urlLength > 150) {
+    version = 6;
+  } else {
+    version = 5;
+  }
+
   const fileName = url.split("?")[0].split("/").pop();
   if (!url) {
     return res.status(400).json({ error: 'Missing "url" parameter' });
@@ -10,13 +25,11 @@ export const qrGenerator = async (req: Request, res: Response) => {
 
   try {
     const qrCode = await qrcode.toBuffer(url, {
+      version,
       width: 400,
       margin: 1,
       color: { dark: "#102059", light: "#EDEDF0" },
       type: "png",
-      maskPattern: 7,
-      scale: 8,
-      version: 2,
     });
     res.setHeader("Content-Type", "image/png");
     res.setHeader(
