8f090ba81e2a0eb7cc1c77d66029ba06b4f1a5f8
francanete <francanetedev@gmail.com>
2023-01-23 12:36:19 +0000

    organise folders and clean up code

--- index.ts
+++ index.ts
@@ -1,41 +1,13 @@
 import express from "express";
-import qrcode from "qrcode";
+import { qrGenerator } from "./src/controller/qrGenerator";
 import { limiter } from "./src/middleware/rateLimit";
 
 const app = express();
 
 app.use(express.json(), limiter);
 
-app.get("/qr-code", async (req, res) => {
-  const { url } = req.query as { url: string };
-  const fileName = url.split("?")[0].split("/").pop();
-  if (!url) {
-    return res.status(400).json({ error: 'Missing "url" parameter' });
-  }
+app.use("/qr-code", qrGenerator);
 
-  try {
-    const qrCode = await qrcode.toBuffer(url, {
-      width: 400,
-      margin: 1,
-      color: { dark: "#102059", light: "#EDEDF0" },
-      type: "png",
-      maskPattern: 7,
-      scale: 8,
-      version: 2,
-    });
-    res.setHeader("Content-Type", "image/png");
-    res.setHeader(
-      "Content-Disposition",
-      `attachment; filename="${fileName}.png"`
-    );
-    return res.status(200).end(qrCode, "binary");
-  } catch (error) {
-    if (error instanceof Error) {
-      return res.status(400).json({ error: error.message });
-    }
-  }
-});
-
-app.listen(3000, () => {
-  console.log("[⚡️Server⚡️] started on port 3000");
+app.listen(8000, () => {
+  console.log("[⚡️Server⚡️] started on port 8000");
 });
---
+++ src/controller/qrGenerator.ts
@@ -0,0 +1,32 @@
+import { Request, Response } from "express";
+import qrcode from "qrcode";
+
+export const qrGenerator = async (req: Request, res: Response) => {
+  const { url } = req.query as { url: string };
+  const fileName = url.split("?")[0].split("/").pop();
+  if (!url) {
+    return res.status(400).json({ error: 'Missing "url" parameter' });
+  }
+
+  try {
+    const qrCode = await qrcode.toBuffer(url, {
+      width: 400,
+      margin: 1,
+      color: { dark: "#102059", light: "#EDEDF0" },
+      type: "png",
+      maskPattern: 7,
+      scale: 8,
+      version: 2,
+    });
+    res.setHeader("Content-Type", "image/png");
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="${fileName}.png"`
+    );
+    return res.status(200).end(qrCode, "binary");
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(400).json({ error: error.message });
+    }
+  }
+};
